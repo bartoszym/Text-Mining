7e94cc5bd9f662201a6ad2baea6f4d3caa2901b7
bartoszym <szymaniak98@gmail.com>
2023-03-16 22:18:07 +0100

    added new functions

--- mining_songs/spacy_services.py
+++ mining_songs/spacy_services.py
@@ -1,6 +1,6 @@
 import spacy
 
-from collections import Counter
+from collections import Counter, defaultdict
 
 
 def lemmatizer_lookup(lyrics: str) -> spacy.tokens.doc.Doc:
@@ -15,8 +15,28 @@ def most_frequent_words(lyrics: str, top_n_words: int = None) -> dict:
         "en_core_web_sm", enable=["tagger", "attribute_ruler", "lemmatizer"]
     )
     doc = nlp(lyrics)
-    cleaned_lyrics2 = [
+    cleaned_lyrics = [
         token.lemma_ for token in doc if not (token.is_stop or not token.is_alpha)
     ]
-    freq_dist = Counter(cleaned_lyrics2)
+    freq_dist = Counter(cleaned_lyrics)
     return dict(freq_dist.most_common(top_n_words))
+
+
+def get_NERS(lyrics: str):
+    nlp = spacy.load("en_core_web_sm")
+    NER_dict = defaultdict(set)
+    doc = nlp(lyrics)
+    interesting_entities = (
+        "PERSON",
+        "GPE",
+        "ORG",
+        "NORP",
+        "MONEY",
+        "WORK_OF_ART",
+        "LOC",
+        "PRODUCT",
+    )
+    for ent in doc.ents:
+        if ent.label_ in interesting_entities:
+            NER_dict[ent.label_].add(ent.text)
+    return NER_dict
